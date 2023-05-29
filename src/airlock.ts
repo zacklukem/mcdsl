4ac9ef4ee92a16fad58e0fc91f821e46fdfe14ff
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2023-05-28 18:46:05 -0700

    initial commit

---
+++ src/airlock.ts
@@ -0,0 +1,165 @@
+import { Coord, coord, Commands, and, D, iff } from "./mcmd";
+
+let c = new Commands("airlock_1");
+
+const pr_out = [5045, 227, 4960]
+const pr_in = [5040, 227, 4960]
+
+const lv_out = [5043, 228, 4959]
+const lv_out_dir = D.WEST;
+
+const lv_in = [5042, 228, 4959]
+const lv_in_dir = D.EAST;
+
+const lv_pres = [5043, 228, 4961]
+const lv_pres_dir = D.NORTH;
+
+const door_out = [5044, 227, 4960]
+const door_in = [5041, 227, 4960]
+
+enum Pressure {
+    PRESURIZED = 0,
+    DEPRESURIZED = 1,
+    PRESSURIZING = 2,
+    DEPRESURIZING = 3,
+}
+
+function above(pos: Coord) {
+    return [pos[0], pos[1] + 1, pos[2]]
+}
+
+function lever_on(pos: Coord) {
+    return `block ${coord(pos)} minecraft:lever[powered=true]`
+}
+
+function lever_off(pos: Coord) {
+    return `block ${coord(pos)} minecraft:lever[powered=false]`
+}
+
+function pr_on(pos: Coord) {
+    return `block ${coord(pos)} minecraft:polished_blackstone_pressure_plate[powered=true]`
+}
+
+function pr_off(pos: Coord) {
+    return `block ${coord(pos)} minecraft:polished_blackstone_pressure_plate[powered=false]`
+}
+
+function set_lever(pos: Coord, dir: D, on: boolean) {
+    return `setblock ${coord(pos)} minecraft:lever[facing=${dir},powered=${on}]`
+}
+
+let pressurized = c.var_int(true, "pressurized")
+
+function close_door(pos: Coord) {
+    return [
+        `setblock ${coord(pos)} minecraft:stone`,
+        `setblock ${coord(above(pos))} minecraft:stone`,
+    ];
+}
+
+function open_door(pos: Coord) {
+    return [
+        `setblock ${coord(pos)} minecraft:air`,
+        `setblock ${coord(above(pos))} minecraft:air`,
+    ];
+}
+
+let pres_trigger = c.mk_trigger();
+
+let time_scale = 1 / 3;
+
+pres_trigger.at(0)
+    .execute(iff(pressurized.eq(Pressure.DEPRESURIZED)))
+        .impulse(pressurized.set(Pressure.PRESSURIZING))
+    .end()
+    .execute(iff(pressurized.eq(Pressure.PRESURIZED)))
+        .impulse(pressurized.set(Pressure.DEPRESURIZING))
+    .end()
+    .impulse("bossbar set minecraft:airlock_1 color red")
+    .impulse("bossbar set minecraft:airlock_1 value 0")
+    .impulse("bossbar set minecraft:airlock_1 visible true")
+
+for (let i = 10; i <= 90; i += 10) {
+    pres_trigger.at(i * time_scale)
+        .impulse(`bossbar set minecraft:airlock_1 value ${i}`)
+}
+
+pres_trigger.at(100 * time_scale)
+    .impulse("bossbar set minecraft:airlock_1 color green")
+    .impulse("bossbar set minecraft:airlock_1 value 100")
+    .impulse(set_lever(lv_pres, lv_pres_dir, false))
+    .execute(iff(pressurized.eq(Pressure.PRESSURIZING)))
+        .impulse(pressurized.set(Pressure.PRESURIZED))
+    .end()
+    .execute(iff(pressurized.eq(Pressure.DEPRESURIZING)))
+        .impulse(pressurized.set(Pressure.DEPRESURIZED))
+    .end()
+
+pres_trigger.at(120 * time_scale)
+    .impulse("bossbar set minecraft:airlock_1 visible false")
+    .impulse("bossbar set minecraft:airlock_1 value 0")
+    .impulse("bossbar set minecraft:airlock_1 color red")
+    .repeat(pres_trigger.reset())
+
+c
+    // Enter from outside, set depressurized
+    .execute(iff(pr_on(pr_out)))
+        .repeat(pressurized.set(Pressure.DEPRESURIZED))
+        .repeat_all(open_door(door_out))
+        .repeat(set_lever(lv_out, lv_out_dir, false))
+    .end()
+
+    // Enter from inside, set pressurized
+    .execute(iff(pr_on(pr_in)))
+        .repeat(pressurized.set(Pressure.PRESURIZED))
+        .repeat_all(open_door(door_in))
+        .repeat(set_lever(lv_in, lv_in_dir, false))
+    .end()
+
+    // close doors
+    .execute(and(iff(pr_off(pr_out)), iff(lever_off(lv_out))))
+        .repeat_all(close_door(door_out))
+    .end()
+
+    .execute(and(iff(pr_off(pr_in)), iff(lever_off(lv_in))))
+        .repeat_all(close_door(door_in))
+    .end()
+
+    // if lv_out is on and not pressurized
+    .execute(and(iff(lever_on(lv_out)), iff(pressurized.eq(Pressure.DEPRESURIZED))))
+        .repeat_all(open_door(door_out))
+    .end()
+
+    // if lv_in is on and pressurized
+    .execute(and(iff(lever_on(lv_in)), iff(pressurized.eq(Pressure.PRESURIZED))))
+        .repeat_all(open_door(door_in))
+    .end()
+
+    .execute(and(iff(lever_on(lv_out)), iff(lever_on(lv_pres))))
+        .repeat(set_lever(lv_pres, lv_pres_dir, false))
+    .end()
+
+    .execute(and(iff(lever_on(lv_in)), iff(lever_on(lv_pres))))
+        .repeat(set_lever(lv_pres, lv_pres_dir, false))
+    .end()
+
+    // if all three levers on and depressurized
+    .execute(and(
+        iff(pressurized.eq(Pressure.DEPRESURIZED)),
+        iff(lever_off(lv_out)),
+        iff(lever_off(lv_in)),
+        iff(lever_on(lv_pres)))
+    )
+        .repeat(pres_trigger.trigger())
+    .end()
+
+    // if all three levers on and depressurized
+    .execute(and(
+        iff(pressurized.eq(Pressure.PRESURIZED)),
+        iff(lever_off(lv_out)),
+        iff(lever_off(lv_in)),
+        iff(lever_on(lv_pres)))
+    )
+        .repeat(pres_trigger.trigger())
+    .end()
+.build([4890,166,4943])
---
+++ src/mcmd.ts
@@ -0,0 +1,317 @@
+export type Coord = number[];
+
+function add(a: Coord, b: Coord): Coord {
+    return [a[0] + b[0], a[1] + b[1], a[2] + b[2]];
+}
+
+function sub(a: Coord, b: Coord): Coord {
+    return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+
+function mul(a: Coord, b: number): Coord {
+    return [a[0] * b, a[1] * b, a[2] * b];
+}
+
+function div(a: Coord, b: number): Coord {
+    return [a[0] / b, a[1] / b, a[2] / b];
+}
+
+export function iff(pred: string): string {
+    return `if ${pred}`
+}
+
+export function unless(pred: string): string {
+    return `unless ${pred}`
+}
+
+export function and(...conditions: string[]): string {
+    return conditions.join(" ")
+}
+
+export function coord(pos: Coord) {
+    return `${pos[0]} ${pos[1]} ${pos[2]}`
+}
+
+export enum D {
+    NORTH = "north",
+    SOUTH = "south",
+    EAST = "east",
+    WEST = "west",
+}
+
+export interface AddCmd {
+    repeat(cmd: string): AddCmd;
+    repeat_all(cmds: string[]): AddCmd;
+    impulse(cmd: string): AddCmd;
+    impulse_all(cmds: string[]): AddCmd;
+}
+
+export class Trigger {
+    times: Map<number, CommandsInner>;
+    ns: string;
+    id: string;
+    ea: number;
+
+    constructor(ns: string, id: string) {
+        this.times = new Map();
+        this.ns = ns;
+        this.id = id;
+        this.ea=0;
+    }
+
+    at(time: number): CommandsInner {
+        if (this.times.has(time)) {
+            return this.times.get(time)!;
+        } else {
+            let c = new CommandsInner(this.ns);
+            this.times.set(time, c);
+            return c;
+        }
+    }
+
+    trigger(): string {
+        return `@t${this.id}%`
+    }
+
+    reset(): string{
+        return `@r${this.id}%`
+    }
+}
+
+export class VarBool {
+    private rep: string;
+    private ns: string
+
+    constructor(id: string, ns: string) {
+        this.rep = id;
+        this.ns = ns;
+    }
+
+    true(): string {
+        return `score ${this.rep} ${this.ns} matches 1`;
+    }
+
+    false(): string {
+        return `score ${this.rep} ${this.ns} matches 0`;
+    }
+
+    set_true(): string {
+        return `scoreboard players set ${this.rep} ${this.ns} 1`;
+    }
+
+    set_false(): string {
+        return `scoreboard players set ${this.rep} ${this.ns} 0`;
+    }
+
+    toggle(): string[] {
+        return [
+            `execute ${iff(this.false())} run ${this.set_true()}`,
+            `execute ${iff(this.true())} run ${this.set_false()}`
+        ];
+    }
+}
+
+export class VarInt {
+    private rep: string;
+    private ns: string
+
+    constructor(id: string, ns: string) {
+        this.rep = id;
+        this.ns = ns;
+    }
+
+    eq(val: number): string {
+        return `score ${this.rep} ${this.ns} matches ${val}`;
+    }
+
+    set(val: number): string {
+        return `scoreboard players set ${this.rep} ${this.ns} ${val}`;
+    }
+}
+
+export class CommandsInner implements AddCmd {
+    protected commands: [boolean, string][]
+    protected ns: string
+
+    constructor(ns: string) {
+        this.ns = ns;
+        this.commands = []
+    }
+
+    repeat(...cmd: string[]): CommandsInner {
+        for (let c of cmd) {
+            this.commands.push([true, c]);
+        }
+        return this;
+    }
+
+    repeat_all(cmds: string[]): CommandsInner {
+        for (let c of cmds) {
+            this.commands.push([true, c]);
+        }
+        return this;
+    }
+
+    impulse(...cmd: string[]): CommandsInner {
+        for (let c of cmd) {
+            this.commands.push([false, c]);
+        }
+        return this;
+    }
+
+    impulse_all(cmds: string[]): CommandsInner {
+        for (let c of cmds) {
+            this.commands.push([false, c]);
+        }
+        return this;
+    }
+
+    execute(...conditions: string[]): ExecuteBuilder<CommandsInner> {
+        return new ExecuteBuilder(this, conditions);
+    }
+
+    var_bool(init: boolean, id?: string): VarBool {
+        return new VarBool(id || crypto.randomUUID(), this.ns);
+    }
+
+    var_int(init: boolean, id?: string): VarInt {
+        return new VarInt(id || crypto.randomUUID(), this.ns);
+    }
+
+    cmds(): [boolean, string][] {
+        return this.commands;
+    }
+}
+
+
+export class Commands extends CommandsInner {
+    private triggers: Map<string, Trigger>;
+    private trigger_id: number;
+
+    constructor(ns: string) {
+        super(ns);
+        this.triggers = new Map();
+        this.trigger_id = 0;
+    }
+
+    override execute(...conditions: string[]): ExecuteBuilder<Commands> {
+        return new ExecuteBuilder(this, conditions);
+    }
+
+    build(root_pos: Coord) {
+        function repeater(pos: Coord, dir: D, delay: number): string {
+            return `setblock ${coord(pos)} minecraft:repeater[facing=${dir},delay=${delay}]`;
+        }
+
+        function cmd_blk(pos: Coord, repeat: boolean, needs_redstone: boolean, cmd: string): string {
+            if (repeat) {
+                return `setblock ${coord(pos)} minecraft:repeating_command_block{Command:"${cmd}",auto:${needs_redstone?0:1}}`;
+            } else {
+                return `setblock ${coord(pos)} minecraft:command_block{Command:"${cmd}",auto:${needs_redstone?0:1}}`;
+            }
+        }
+
+        let out = [];
+
+        let trigger_map: Map<string, [Coord, Coord]> = new Map();
+
+        // Trigggers
+        let x = 0;
+        let start_x = x;
+        let start_z = 3;
+        let end_z = 3;
+        for (let [id, trigger] of this.triggers.entries()) {
+            let start_x = x;
+            for (let [time, commands] of trigger.times.entries()) {
+                for (let [repeat, command] of commands.cmds()) {
+                    let t = Math.round(time);
+                    let n = 3;
+                    while (t > 0) {
+                        out.push(repeater(add(root_pos, [x, 0, n]), D.NORTH, Math.min(t, 4)));
+                        t -= Math.min(t, 4);
+                        n++;
+                    }
+                    end_z = Math.max(end_z, n);
+                    out.push(cmd_blk(add(root_pos, [x, 0, n]), repeat, true, command));
+                    x += 2;
+                }
+            }
+            let end_x = x;
+            trigger_map.set(id, [add(root_pos, [start_x, 0, 2]), add(root_pos, [end_x, 0, 2])]);
+        }
+        let end_x = x;
+        out.unshift(`fill ${coord(add(root_pos, [start_x, 0, start_z - 1]))} ${coord(add(root_pos, [end_x, 0, end_z]))} minecraft:air`)
+        out.unshift(`fill ${coord(add(root_pos, [start_x, -1, start_z]))} ${coord(add(root_pos, [end_x, -1, end_z]))} minecraft:gray_wool`)
+
+
+        // Repeats
+        x = 0;
+        for (let [repeat, command] of this.commands) {
+            out.push(cmd_blk(add(root_pos, [x, 0, 0]), repeat, false, command));
+            x++;
+        }
+
+        for (let i in out) {
+            for (let [id, trig] of trigger_map.entries()) {
+                out[i] = out[i].replaceAll(
+                    `@t${id}%`, `fill ${coord(trig[0])} ${coord(trig[1])} minecraft:redstone_block`
+                );
+                out[i] = out[i].replaceAll(
+                    `@r${id}%`, `fill ${coord(trig[0])} ${coord(trig[1])} minecraft:brown_wool`
+                );
+            }
+        }
+
+        for (let cmd of out) {
+            console.log(cmd);
+        }
+    }
+
+    mk_trigger(): Trigger {
+        let t = new Trigger(this.ns, (this.trigger_id++).toString());
+        this.triggers.set(t.id, t);
+        return t;
+    }
+}
+
+export class ExecuteBuilder<T extends AddCmd> implements AddCmd {
+    c: T;
+    conditions: string[];
+
+    constructor(c: T, conditions: string[]) {
+        this.c = c;
+        this.conditions = conditions;
+    }
+
+    repeat(cmd: string): ExecuteBuilder<T> {
+        for (let condition of this.conditions) {
+            this.c.repeat(`execute ${condition} run ${cmd}`);
+        }
+        return this;
+    }
+
+    repeat_all(cmds: string[]): ExecuteBuilder<T> {
+        for (let condition of this.conditions) {
+            this.c.repeat_all(cmds.map(c => `execute ${condition} run ${c}`));
+        }
+        return this;
+    }
+
+    impulse(cmd: string): ExecuteBuilder<T> {
+        for (let condition of this.conditions) {
+            this.c.impulse(`execute ${condition} run ${cmd}`);
+        }
+        return this;
+    }
+
+    impulse_all(cmds: string[]): ExecuteBuilder<T> {
+        for (let condition of this.conditions) {
+            this.c.impulse_all(cmds.map(c => `execute ${condition} run ${c}`));
+        }
+        return this;
+    }
+
+    end(): T {
+        return this.c;
+    }
+}
